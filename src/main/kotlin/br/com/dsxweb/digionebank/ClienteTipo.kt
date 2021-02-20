3490d6e3ac69bdd8c80d15f1370b9610df587707
Dario Salles <dariosalles@gmail.com>
2021-02-20 19:22:13 -0300

    Third commit

---
+++ src/main/kotlin/br/com/dsxweb/digionebank/ClienteTipo.kt
@@ -0,0 +1,11 @@
+package br.com.dsxweb.digionebank
+
+enum class ClienteTipo(val descricao: String) {
+
+    PF("Pessoa Física"),
+    PJ("Pessoa Jurídica");
+
+    companion object{
+        fun printDescricao() = ""
+    }
+}
---
+++ src/main/kotlin/br/com/dsxweb/digionebank/testes/ClienteTIpoEnumTeste.kt
@@ -0,0 +1,13 @@
+package br.com.dsxweb.digionebank.testes
+
+import br.com.dsxweb.digionebank.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+}
